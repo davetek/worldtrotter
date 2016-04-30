75b33e20c0f3bdee28eebed726c76ae5ccdad274
David Lawrence <dlawrence@focusedconcepts.com>
2016-04-30 07:18:08 -0400

    WorldTrotter v4.0 b2: Changing version # to match chapter in iOS Programming book. Build # will describe how many builds have been done in the current chapter. In this commit, added a new IBAction to the view controller to dismiss the keyboard when the background view is tapped. To trigger this function (method), added a Tap Gesture Recognizer to the main storyboard and connected it to the view controller and to the new IBAction. Ran the app on my phone and confirmed that tapping the background view does dismiss the keyboard.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     
     @IBAction func farenheitFieldEditingChanged(textField: UITextField) {
         if let text = textField.text where !text.isEmpty {
@@ -17,5 +18,9 @@ class ConversionViewController: UIViewController {
         } else {
             celsiusLabel.text = "???"
         }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject) {
+        textField.resignFirstResponder()
     }
 }

92be91f35422031ad52199412fb64dba2ff911a5
David Lawrence <dlawrence@focusedconcepts.com>
2016-04-30 08:19:18 -0400

    WorldTrotter v4.0 b2: computes a Celsius temperature from the Farenheit temperature entered in the text field and displayed in the label, using a computed value for the Celsius temp. Also adds a property observer (didSet) to the farenheitValue variable that is called after that property value changes; this property observer calls the updateCelsiusLabel() method. Then update the farenheitValue variable when the text field changes, which triggers the property observer, which calls the method to update the Celsius label, which references the Celsius value which is computed based on the Farenheit value. I noted that the Celsius value appears as a two-digit number with one decimal place if the Farenheit value entered is 32 or has a decimal value of .5. Otherwise the number of digits after the decimal run off the screen.

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -10,17 +10,42 @@ import UIKit
 
 class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
+    var farenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = farenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
     @IBOutlet var textField: UITextField!
     
     @IBAction func farenheitFieldEditingChanged(textField: UITextField) {
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
-        } else {
-            celsiusLabel.text = "???"
+        if let text = textField.text, value = Double(text) {
+            farenheitValue = value
+        }
+        else {
+            farenheitValue = nil
         }
     }
     
     @IBAction func dismissKeyboard(sender: AnyObject) {
         textField.resignFirstResponder()
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
     }
 }

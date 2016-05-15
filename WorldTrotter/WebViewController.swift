acc8cff9f261f3319ca5011bbabbea5ad97318b9
David Lawrence <dlawrence@focusedconcepts.com>
2016-05-15 09:18:17 -0400

    WorldTrotter v6.0 b4.1: deleted web view controller and started over, adding a new view controller via storyboard, setting it up as instructed in View Controllers chapter for Map View Controller, but without adding a view. Then programmatically add the map view, run the app, and it works! The use code researched yesterday, to load the Big Nerd Ranch website. Run the app; the app does not crash, but the site does not load because it is blocked by security.

--- WorldTrotter/WebViewController.swift
+++ WorldTrotter/WebViewController.swift
@@ -2,7 +2,7 @@
 //  WebViewController.swift
 //  WorldTrotter
 //
-//  Created by David Lawrence on 5/14/16.
+//  Created by David Lawrence on 5/15/16.
 //  Copyright © 2016 focusedConcepts. All rights reserved.
 //
 
@@ -10,27 +10,34 @@ import UIKit
 
 class WebViewController: UIViewController {
     
-    @IBOutlet var webView: UIWebView!
-    var URLPath = "http://www.bignerdranch.com"
+    var webView: UIWebView!
+    
+    override func loadView() {
+        //create a web view
+        webView = UIWebView()
+        
+        //set it as the primary view of this view controller
+        view = webView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         print("WebViewController loaded its view")
         
-        view.addSubview(webView)
-        loadURL()
-        
-    }
-    
-    func loadURL() {
-        let requestURL = NSURL(string:URLPath)
+        let requestURL = NSURL(string:"http://www.bignerdranch.com")
         print(requestURL)
+        
         let request = NSURLRequest(URL: requestURL!)
         print(request)
+        
         if webView != nil {
             webView.loadRequest(request)
         } else {
             print("webView is nil")
         }
+        
+        
+        
     }
 }
